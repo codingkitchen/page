96d22bc60c6fdf06ae63cef5cbae4e911f242074
Konrad Kühne <konrad.kuehne@rocketmail.com>
2015-11-06 16:15:13 +0100

    add heroku port var

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ var server = require("./src/server/core")
 var db = require("./src/server/database")
 
 var STATIC_PATH = __dirname + "/dist"
-var SERVER_PORT = 8080
+var SERVER_PORT = process.env.PORT || 8080
 
 db.init()
 server.startServer(SERVER_PORT, STATIC_PATH)
